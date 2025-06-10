057ea65aaf5f22c88a7d11d55d56e862b4aca382
Zaved <zavedahmadca707@gmail.com>
2025-06-10 19:27:24 +0530

    using coil AsyncImagePainter instead of AsyncImage also centered everything and made a little cleaner

--- app/src/main/java/com/zavedahmad/randomduck/MainActivity.kt
+++ app/src/main/java/com/zavedahmad/randomduck/MainActivity.kt
@@ -5,17 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import com.zavedahmad.randomduck.ui.theme.RandomDuckTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,12 +41,30 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val link by viewModel.link.collectAsStateWithLifecycle()
+            val painter = rememberAsyncImagePainter(link)
+            val state by painter.state.collectAsStateWithLifecycle()
             RandomDuckTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)){
-                        AsyncImage(model= link, contentDescription = "random Duck")
+                    Column(modifier = Modifier.padding(innerPadding).fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        OutlinedCard (modifier = Modifier.size(200.dp), ) {
+                            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+                           when(state){
+                               is AsyncImagePainter.State.Empty,
+                               is AsyncImagePainter.State.Loading ->{
+                                   CircularProgressIndicator()
+                               }
+                               is AsyncImagePainter.State.Success -> {
+                                   Image(modifier = Modifier.fillMaxSize(),
+                                       painter = painter,
+                                       contentDescription = "duck"
+                                   )
+                               }
+                               else -> {Text("error")}
+                           }}
+                        }
+
                         Text(link)
-                        Button(onClick = {viewModel.getLinkToImageRandom()}) {Text("getData") }
+                        Button(onClick = { viewModel.getLinkToImageRandom() }) { Text("getData") }
                     }
                 }
             }
